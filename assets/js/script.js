36e27ab304932981adc40dd9545cad48a1b9faa4
WillyAyala35 <wallwash2502@gmail.com>
2024-03-31 00:40:22 -0300

    commit dos

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,21 @@ precio = 400000
 
 precioSpan = document.querySelector(".precio-inicial");
 precioSpan.innerHTML = precio
+
+const cantidad = document.querySelector(".cantidad");
+console.log(cantidad.innerHTML); 
+const sumar = document.querySelector("#sumar");
+const restar = document.querySelector("#restar");
+const total = document.querySelector(".valor-total");
+console.log(total.innerHTML);
+
+sumar.addEventListener('click', () => {
+    cantidad.innerHTML = +cantidad.innerHTML+1
+    total.innerHTML = +cantidad.innerHTML*+precioSpan.innerHTML
+})
+restar.addEventListener('click', () => {
+    if (+cantidad.innerHTML>0) {
+    cantidad.innerHTML = +cantidad.innerHTML-1
+    total.innerHTML = +cantidad.innerHTML*+precioSpan.innerHTML    
+    }
+})
